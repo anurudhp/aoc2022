e1277fae0e0bb2a27b58bcd8785a84fe72f3d40f
anurudhp <anurudhp97@gmail.com>
2022-12-10 01:00:20 +0100

    remove `nub`, use `eraseDups` instead.

--- Aoc/Day06.lean
+++ Aoc/Day06.lean
@@ -4,7 +4,7 @@ import Aoc.Lib.List
 namespace Day06
 
 def findMarker (l : Nat) (s : List Char) : Nat :=
-  if (s.take l |>.nub |>.length) == l then l
+  if (s.take l |>.eraseDups |>.length) == l then l
   else match s with 
   | _ :: s => 1 + findMarker l s
   | _ => 0
