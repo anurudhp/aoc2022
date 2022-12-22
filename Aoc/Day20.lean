ca5e7b6921031057cf17af82c48aaf6ba92587b7
anurudhp <anurudhp97@gmail.com>
2022-12-22 23:31:50 +0100

    day 20

--- Aoc/Day20.lean
+++ Aoc/Day20.lean
@@ -2,22 +2,34 @@ import Aoc
 
 abbrev Message := Array (Int × Nat)
 
-def Message.mix (xs : Message) (p : Nat) : Message := Option.get! do
+def Message.mixₚ (xs : Message) (p : Nat) : Message := Option.get! do
   let i ← xs.findIdx? (·.snd |> (· == p))
   let (x, _) ← xs[i]?
   let xs := xs.eraseIdx i
   let i' := i + x |>.mod xs.size |>.add xs.size |>.mod xs.size |>.toNat
   xs.insertAt i' (x, p)
 
+def Message.mix (xs : Message) : Message := List.range xs.size |>.foldl .mixₚ xs
+
+def Message.decrypt (rounds : Nat) (xs : Message) : Int := Id.run do
+  let mut xs := xs
+  for _ in [:rounds] do xs := xs.mix
+  let xs' := xs.map (·.fst)
+  let ix₀ := xs'.getIdx? 0 |>.get!
+  [1000, 2000, 3000]
+    |>.map (· + ix₀ |>.mod xs.size |> xs'.get!)
+    |>.foldl Int.add 0
+
 def main : IO Unit := IO.interact $ λ input =>
   let xs := lines input |>.map String.toInt!
   let n := xs.length
   let xs : Message := Array.mk <| xs.zip <| List.range n
 
-  let xs' := List.range n |>.foldl Message.mix xs |>.map (·.fst)
-  let ix₀ := xs'.getIdx? 0 |>.get!
-  let dec₁ := [1000, 2000, 3000]
-    |>.map (· + ix₀ |>.mod n |> xs'.get!)
-    |>.foldl Int.add 0
+  -- part 1
+  let dec₁ := xs.decrypt 1
+
+  -- part 2
+  let key := 811589153
+  let dec₂ := xs.map (λ (x, i) => (x * key, i)) |> Message.decrypt 10
   
-  s!"{dec₁}"
+  s!"{dec₁}, {dec₂}"
