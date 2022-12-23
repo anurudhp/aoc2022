39048aee549fc52b5dc1b8b7e2afb5f0b352e835
anurudhp <anurudhp97@gmail.com>
2022-12-23 14:04:10 +0100

    day 18 (fixes #2)

--- Aoc/Day18.lean
+++ Aoc/Day18.lean
@@ -13,10 +13,37 @@ def Pos3.neighbours : Pos3 → List Pos3
   , (x, y, z + 1)
   ]
 
+def Pos3.visible (ps : List Pos3) : Pos3 → Bool
+| (x, y, z) =>
+  [ ps.find? (λ (x', y', z') => x' >= x && y' == y && z' == z)
+  , ps.find? (λ (x', y', z') => x' <= x && y' == y && z' == z)
+  , ps.find? (λ (x', y', z') => x' == x && y' >= y && z' == z)
+  , ps.find? (λ (x', y', z') => x' == x && y' <= y && z' == z)
+  , ps.find? (λ (x', y', z') => x' == x && y' == y && z' >= z)
+  , ps.find? (λ (x', y', z') => x' == x && y' == y && z' <= z)
+  ].any (·.isNone)
+
+def reachable (air ps : List Pos3) : List Pos3 := Id.run do
+  let air := air.eraseDups
+  let mut Q := air.filter (·.visible ps)
+  let mut out := []
+  while not Q.isEmpty do
+    let p := Q.head!
+    Q := Q.tail!
+    out := p :: out
+
+    let ns := p.neighbours
+      |>.filter air.elem
+      |>.filter (not ∘ out.elem)
+    Q := ns ++ Q
+
+  out
+
 def main : IO Unit := IO.interact $ λ input =>
-  let pts : List Pos3 := lines input
+  let ps : List Pos3 := lines input
     |>.map (·.splitOn "," |>.map (·.toNat!) |>.first3!) 
 
-  let air := pts.map (·.neighbours |>.filter (not ∘ pts.elem)) |>.join
+  let air := ps.mapJoin (·.neighbours) |>.filter (not ∘ ps.elem)
+  let outside := reachable air ps
 
-  s!"{air.length}"
+  s!"{air.length}, {air.count outside.elem}"
