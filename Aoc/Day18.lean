9c7432ffae3240a98f833daa8d34b8521cd30353
anurudhp <anurudhp97@gmail.com>
2022-12-23 12:52:58 +0100

    18.1 refactor

--- Aoc/Day18.lean
+++ Aoc/Day18.lean
@@ -3,8 +3,6 @@ import Aoc.Lib.List
 
 abbrev Pos3 := Nat × Nat × Nat
 
-def List.sum (l : List Nat) : Nat := l.foldl .add 0
-
 def Pos3.neighbours : Pos3 → List Pos3 
 | (x, y, z) => 
   [ (x + 1, y, z)
@@ -15,22 +13,10 @@ def Pos3.neighbours : Pos3 → List Pos3
   , (x, y, z + 1)
   ]
 
-def List.isPocket (ps : List Pos3) : Pos3 → Bool
-| (x, y, z) =>
-  [ ps.find? (λ (x', y', z') => x' >= x && y' == y && z' == z)
-  , ps.find? (λ (x', y', z') => x' <= x && y' == y && z' == z)
-  , ps.find? (λ (x', y', z') => x' == x && y' >= y && z' == z)
-  , ps.find? (λ (x', y', z') => x' == x && y' <= y && z' == z)
-  , ps.find? (λ (x', y', z') => x' == x && y' == y && z' >= z)
-  , ps.find? (λ (x', y', z') => x' == x && y' == y && z' <= z)
-  ].all (·.isSome)
-
 def main : IO Unit := IO.interact $ λ input =>
   let pts : List Pos3 := lines input
     |>.map (·.splitOn "," |>.map (·.toNat!) |>.first3!) 
 
-  let area check := 
-    let blocked := pts.map (·.neighbours |>.count check) |>.sum
-    pts.length * 6 - blocked
+  let air := pts.map (·.neighbours |>.filter (not ∘ pts.elem)) |>.join
 
-  s!"{area pts.elem}, {area pts.isPocket}"
+  s!"{air.length}"
