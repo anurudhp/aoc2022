e8a925090b41ec4c77f2d831334ae7127fe98346
anurudhp <anurudhp97@gmail.com>
2022-12-27 18:52:28 +0100

    day 22 (fixes #6)

--- Aoc/Day22.lean
+++ Aoc/Day22.lean
@@ -29,14 +29,13 @@ def mkMoves (s : String) : Array Move := Id.run do
 
   moves.push <| .Walk n 
 
-inductive Dir := | Right | Down | Left | Up
-deriving Inhabited
+inductive Dir := | Rt | Dn | Lt | Up
 
 def Dir.rotCW : Dir → Dir
-| Right => Down
-| Down  => Left
-| Left  => Up
-| Up    => Right
+| Rt => Dn
+| Dn => Lt
+| Lt => Up
+| Up => Rt
 
 def Dir.rotate (d : Dir) : Move → Dir
 | .R => d.rotCW
@@ -44,21 +43,21 @@ def Dir.rotate (d : Dir) : Move → Dir
 | _  => d
 
 def Dir.toPasswd : Dir → Nat
-| .Right => 0
-| .Down  => 1
-| .Left  => 2
-| .Up    => 3
+| .Rt => 0
+| .Dn => 1
+| .Lt => 2
+| .Up => 3
 
 abbrev Pos := Nat × Nat × Dir
 
 def Pos.toPasswd : Pos → Nat
 | (x, y, d) => x * 1000 + y * 4 + d.toPasswd
 
-def Grid.walk (g : Grid) : Pos → Pos 
-| p@(x, y, d) =>
+def Grid.next₁ (g : Grid) : Pos → Pos 
+| (x, y, d) =>
   let x' := 
     match d with
-    | .Down =>
+    | .Dn =>
       if g.at (x + 1) y ≠ ' '
       then x + 1
       else g.findIdx? (·.get ⟨y⟩ ≠ ' ') |>.get!
@@ -69,27 +68,59 @@ def Grid.walk (g : Grid) : Pos → Pos
     | _ => x
   let y' := 
     match d with
-    | .Right =>
+    | .Rt =>
       if g.at x (y + 1) ≠ ' '
       then y + 1
       else g[x]!.firstIdx (· ≠ ' ') 
-    | .Left  =>
+    | .Lt  =>
       if g.at x (y - 1) ≠ ' '
       then y - 1
       else g[x]!.lastIdx (· ≠ ' ') 
     | _ => y
-  if g.at x' y' == '.' then (x', y', d) else p
+  (x', y', d)
 
-def Grid.move (g : Grid) : Pos → Move → Pos 
-| p, .Walk n => n.repeat g.walk p
+/- cube structure:
+.AB
+.C.
+DE.
+F..  -/
+
+macro_rules | `($x ∈ [ $l ... $r ]) => `($l ≤ $x && $x ≤ $r)
+
+def Grid.next₂ (g : Grid) : Pos → Pos | p@(x, y, d) => Id.run do
+  if let .Up := d then
+    if x == 1   && y ∈ [ 51 ... 100] then return (y + 100,       1, .Rt) -- A → F
+    if x == 1   && y ∈ [101 ... 150] then return (    200, y - 100, .Up) -- B → F
+    if x == 101 && y ∈ [  1 ...  50] then return ( y + 50,      51, .Rt) -- D → C
+
+  if let .Dn := d then
+    if x == 50  && y ∈ [101 ... 150] then return ( y - 50,     100, .Lt) -- B → C
+    if x == 150 && y ∈ [ 51 ... 100] then return (y + 100,      50, .Lt) -- E → F
+    if x == 200 && y ∈ [  1 ...  50] then return (      1, y + 100, .Dn) -- F → B
+    
+  if let .Rt := d then
+    if x ∈ [  1 ...  50] && y == 150 then return (151 - x,     100, .Lt) -- B → E
+    if x ∈ [ 51 ... 100] && y == 100 then return (     50,  x + 50, .Up) -- C → B
+    if x ∈ [101 ... 150] && y == 100 then return (151 - x,     150, .Lt) -- E → B
+    if x ∈ [151 ... 200] && y == 50  then return (    150, x - 100, .Up) -- F → E
+
+  if let .Lt := d then
+    if x ∈ [101 ... 150] && y == 1   then return (151 - x,      51, .Rt) -- D → A
+    if x ∈ [151 ... 200] && y == 1   then return (      1, x - 100, .Dn) -- F → A
+    if x ∈ [ 51 ... 100] && y == 51  then return (    101,  x - 50, .Dn) -- C → D
+    if x ∈ [  1 ...  50] && y == 51  then return (151 - x,       1, .Rt) -- A → D
+
+  g.next₁ p
+
+def Grid.step (g : Grid) (next : Pos → Pos) (p : Pos) : Pos :=
+  let p'@(x, y, _) := next p
+  if g.at x y == '.' then p' else p
+
+def Grid.play (g : Grid) (next : Pos → Pos) : Pos → Move → Pos 
+| p, .Walk n => n.repeat (g.step next) p
 | (x, y, d), rot => (x, y, d.rotate rot)
 
-instance : ToString Move where toString | .Walk n => s!"(Walk {n})" | .L => "(Turn L)" | .R => "(Turn R)"
-instance : ToString Dir where toString | .Right => ">" | .Down => "v" | .Left => "<" | .Up => "^"
-
--- def main : IO Unit := IO.interact $ λ input =>
-def main : IO Unit := do
-  let input ← IO.readInput 10000000
+def main : IO Unit := IO.interact $ λ input =>
   let input := lines input |>.reverse
   let moves : Array Move := mkMoves input.head!
   
@@ -100,6 +131,6 @@ def main : IO Unit := do
     |>.push ("".pushn ' ' (m + 1))
     |>.insertAt 0 ("".pushn ' ' (m + 1))
 
-  let src := (1, grid[1]!.firstIdx (· == '.'), .Right)
-  let dst := moves.foldl grid.move src
-  IO.println s!"{dst.toPasswd}"
+  let src := (1, grid[1]!.firstIdx (· == '.'), .Rt)
+  let passwd next := moves.foldl (grid.play next) src |>.toPasswd
+  s!"{passwd grid.next₁}, {passwd grid.next₂}"
